8d57f6996542c815d656c22542b1ac4294a2b94a
Nikita Shvachko <228nikitalik@gmail.com>
2024-05-04 22:40:40 +0300

    4
    Signed-off-by: Nikita Shvachko <228nikitalik@gmail.com>

---
+++ lab2/test/main.cpp
@@ -0,0 +1,58 @@
+#include <gtest/gtest.h>
+
+// Function to check if a tree is a binary search tree
+bool isBST(Node* node, Node* l=nullptr, Node* r=nullptr) {
+    if (node == nullptr)
+        return true;
+    if (l != nullptr && node->key <= l->key)
+        return false;
+    if (r != nullptr && node->key >= r->key)
+        return false;
+    return isBST(node->left, l, node) && isBST(node->right, node, r);
+}
+
+// Function to check if a tree is a heap
+bool isHeap(Node* node) {
+    if (node == nullptr)
+        return true;
+    if (node->left != nullptr && node->left->priority > node->priority)
+        return false;
+    if (node->right != nullptr && node->right->priority > node->priority)
+        return false;
+    return isHeap(node->left) && isHeap(node->right);
+}
+
+TEST(TreapTest, InsertionTest) {
+Node* root = nullptr;
+root = insert(root, 4);
+root = insert(root, 2);
+root = insert(root, 6);
+root = insert(root, 1);
+root = insert(root, 3);
+root = insert(root, 5);
+root = insert(root, 7);
+
+ASSERT_TRUE(isBST(root));
+ASSERT_TRUE(isHeap(root));
+}
+
+TEST(TreapTest, DeletionTest) {
+Node* root = nullptr;
+root = insert(root, 4);
+root = insert(root, 2);
+root = insert(root, 6);
+root = insert(root, 1);
+root = insert(root, 3);
+root = insert(root, 5);
+root = insert(root, 7);
+
+root = erase(root, 4);
+
+ASSERT_TRUE(isBST(root));
+ASSERT_TRUE(isHeap(root));
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ lab2/treap.h
@@ -0,0 +1,138 @@
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+struct Node {
+    int key, priority;
+    Node *left, *right, *parent;
+
+    Node(int k) : key(k), priority(rand()), left(nullptr), right(nullptr), parent(nullptr) {}
+};
+
+void rotateRight(Node *&root, Node *x) {
+    Node *y = x->left;
+    x->left = y->right;
+    if (y->right) {
+        y->right->parent = x;
+    }
+    y->parent = x->parent;
+    if (!x->parent) {
+        root = y;
+    } else if (x == x->parent->right) {
+        x->parent->right = y;
+    } else {
+        x->parent->left = y;
+    }
+    y->right = x;
+    x->parent = y;
+}
+
+void rotateLeft(Node *&root, Node *x) {
+    Node *y = x->right;
+    x->right = y->left;
+    if (y->left) {
+        y->left->parent = x;
+    }
+    y->parent = x->parent;
+    if (!x->parent) {
+        root = y;
+    } else if (x == x->parent->left) {
+        x->parent->left = y;
+    } else {
+        x->parent->right = y;
+    }
+    y->left = x;
+    x->parent = y;
+}
+
+
+Node *insert(Node *root, int key) {
+    Node *newNode = new Node(key);
+    if (!root) return newNode;
+
+    Node *curr = root;
+    Node *parent = nullptr;
+    while (curr) {
+        parent = curr;
+        if (key < curr->key) {
+            curr = curr->left;
+        } else {
+            curr = curr->right;
+        }
+    }
+
+    if (!parent) {
+        return newNode;
+    }
+
+    if (key < parent->key) {
+        parent->left = newNode;
+    } else {
+        parent->right = newNode;
+    }
+    newNode->parent = parent;
+
+
+    while (newNode->parent && newNode->priority > newNode->parent->priority) {
+        if (newNode == newNode->parent->left) {
+            rotateRight(root, newNode->parent);
+        } else {
+            rotateLeft(root, newNode->parent);
+        }
+    }
+
+
+    return (parent == nullptr) ? newNode : root;
+}
+
+
+Node *erase(Node *&root, int key) {
+    Node *curr = root;
+    Node *parent = nullptr;
+
+
+    while (curr && curr->key != key) {
+        parent = curr;
+        if (key < curr->key) {
+            curr = curr->left;
+        } else {
+            curr = curr->right;
+        }
+    }
+
+    if (!curr) return root;
+
+
+    while (curr->left || curr->right) {
+        if (!curr->right || (curr->left && curr->left->priority > curr->right->priority)) {
+            rotateRight(root, curr);
+            curr = curr->parent;
+        } else {
+            rotateLeft(root, curr);
+            curr = curr->parent;
+        }
+    }
+
+
+    if (parent) {
+        if (parent->left == curr) {
+            parent->left = nullptr;
+        } else {
+            parent->right = nullptr;
+        }
+    } else {
+        root = nullptr;
+    }
+    delete curr;
+
+    return root;
+}
+
+
+void printTreap(Node *root) {
+    if (!root) {
+        return;
+    }
+    printTreap(root->left);
+    cout << root->key << " " << endl;
+    printTreap(root->right);
+}
